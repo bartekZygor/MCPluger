c32d43c769f5973252e1cdb9738165c0b408e235
Baartosz <bb@ww.com>
2025-07-18 20:27:39 +0200

    dodanie projektu

---
+++ MCPluger/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿using System.Drawing;
+
+namespace MCPluger
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.MistyRose;
+            this.label1.Location = new System.Drawing.Point(150, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MCPluger";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.MistyRose;
+            this.label2.Location = new System.Drawing.Point(-1, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "FOLDER WITH MODS TO PLUG";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.MistyRose;
+            this.label3.Location = new System.Drawing.Point(145, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "GTA 5 FOLDER:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(200, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "PLUG OUT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(120, 150);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "PLUG IN";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(148, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 20);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "SELECT PATH";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.Location = new System.Drawing.Point(157, 102);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 20);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "SELECT PATH";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.ForeColor = System.Drawing.Color.MistyRose;
+            this.label4.Location = new System.Drawing.Point(120, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.ForeColor = System.Drawing.Color.MistyRose;
+            this.label5.Location = new System.Drawing.Point(120, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 8;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.Gray;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.linkLabel1.Font = new System.Drawing.Font("MS Reference Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.linkLabel1.LinkColor = System.Drawing.Color.Silver;
+            this.linkLabel1.Location = new System.Drawing.Point(-1, 174);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(111, 11);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "APOKI All Rights Reserved";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 186);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "MCPluger by APOKI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
---
+++ MCPluger/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Net;
+using System.Diagnostics;
+
+namespace MCPluger
+{
+    public partial class Form1 : Form
+    {
+        SoundPlayer player;
+        public Form1()
+        {
+            InitializeComponent();
+            CenterLabel();
+            this.Resize += Form1_Resize;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+
+            //music
+            string url = "https://github.com/bartekZygor/MCPluger/raw/refs/heads/main/space.wav"; // <-- zmień na swój URL
+            PlayWavFromUrl(url);
+
+
+            string folder = Application.StartupPath;
+
+            string sciezka = Path.Combine(folder, "background.png");
+
+            if (File.Exists(sciezka))
+            {
+                this.BackgroundImage = Image.FromFile(sciezka);
+                this.BackgroundImageLayout = ImageLayout.Stretch; // dopasowanie rozmiaru
+            }
+        }
+
+        private void CenterLabel()
+        {
+            int centerX1 = (this.ClientSize.Width - label1.Width) / 2;
+            int centerX2 = (this.ClientSize.Width - label2.Width) / 2;
+            int centerX3 = (this.ClientSize.Width - label3.Width) / 2;
+            int centerX6 = (this.ClientSize.Width - button3.Width) / 2;
+            int centerX7 = (this.ClientSize.Width - button4.Width) / 2;
+
+            label1.Location = new Point(centerX1, label1.Location.Y);
+            label2.Location = new Point(centerX2, label2.Location.Y);
+            label3.Location = new Point(centerX3, label3.Location.Y);
+            button3.Location = new Point(centerX6, button3.Location.Y);
+            button4.Location = new Point(centerX7, button4.Location.Y);
+        }
+
+        private void button2_Click(object sender, EventArgs e) // PLUG IN
+        {
+            string gtaPath = label4.Text;
+            string modsSourcePath = label5.Text;
+            string modsTargetPath = Path.Combine(gtaPath, "mods");
+
+            if (!Directory.Exists(gtaPath) || !Directory.Exists(modsSourcePath))
+            {
+                MessageBox.Show("Invalid paths specified.");
+                return;
+            }
+
+            if (!Directory.Exists(modsTargetPath))
+            {
+                Directory.CreateDirectory(modsTargetPath);
+            }
+
+            string[] files = Directory.GetFiles(modsSourcePath);
+
+            foreach (string file in files)
+            {
+                string fileName = Path.GetFileName(file);
+                string destination = Path.Combine(modsTargetPath, fileName);
+                File.Copy(file, destination, true);
+            }
+
+            MessageBox.Show("Files have been added to GTA (mods).");
+        }
+
+        private void button1_Click(object sender, EventArgs e) // PLUG OUT
+        {
+            string gtaPath = label4.Text;
+            string modsTargetPath = label5.Text;
+            string modsSourcePath = Path.Combine(gtaPath, "mods");
+
+            if (!Directory.Exists(modsSourcePath) || !Directory.Exists(modsTargetPath))
+            {
+                MessageBox.Show("Missing mods folder or destination folder.");
+                return;
+            }
+
+            string[] files = Directory.GetFiles(modsSourcePath);
+
+            foreach (string file in files)
+            {
+                string fileName = Path.GetFileName(file);
+                string destination = Path.Combine(modsTargetPath, fileName);
+
+                if (File.Exists(destination))
+                {
+                    File.Delete(destination);
+                }
+
+                File.Move(file, destination);
+            }
+
+            MessageBox.Show("Mod(s) removed from GTA and migrated back. No data was lost.");
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                dialog.Description = "Select GTA 5 FOLDER (path1)";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    label4.Text = dialog.SelectedPath;
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                dialog.Description = "Select Folder with mods (path2)";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    label5.Text = dialog.SelectedPath;
+                }
+            }
+        }
+
+        private void PlayWavFromUrl(string url)
+        {
+            try
+            {
+                string tempPath = Path.Combine(Path.GetTempPath(), "temp_music.wav");
+
+                using (WebClient wc = new WebClient())
+                {
+                    wc.DownloadFile(url, tempPath);
+                }
+
+                player = new SoundPlayer(tempPath);
+                player.PlayLooping();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+
+            if (player != null)
+            {
+                player.Stop();
+                player.Dispose();
+            }
+
+            try
+            {
+                string tempPath = Path.Combine(Path.GetTempPath(), "temp_music.wav");
+                if (File.Exists(tempPath))
+                    File.Delete(tempPath);
+            }
+            catch { }
+        }
+
+
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            CenterLabel();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            linkLabel1.Links.Add(0, linkLabel1.Text.Length, "https://apoki.site");
+        }
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string url = e.Link.LinkData as string;
+            if (!string.IsNullOrEmpty(url))
+            {
+                try
+                {
+                    Process.Start(new ProcessStartInfo
+                    {
+                        FileName = url,
+                        UseShellExecute = true
+                    });
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Nie udało się otworzyć linku: " + ex.Message);
+                }
+            }
+        }
+
+    }
+}
